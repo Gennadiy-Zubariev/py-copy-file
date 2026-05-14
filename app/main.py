fe9d2f5fc4a902ffec9e3b5f913be93d6016bb00
Gena <genada89@gmail.com>
2026-05-14 16:20:19 +0300

    fix conversation delete print

--- app/main.py
+++ app/main.py
@@ -2,15 +2,14 @@ def copy_file(command: str) -> None:
     try:
         com, original_file, copy_file = command.split()
     except ValueError as e:
-        print(f"Invalid command format: {e}")
         return
 
     if com == "cp" and original_file != copy_file:
         try:
-            with open(original_file, "r") as original_file, open(
+            with open(original_file, "r") as src_file, open(
                 copy_file, "w"
-            ) as copy_file:
-                content = original_file.read()
-                copy_file.write(content)
+            ) as dst_file:
+                content = src_file.read()
+                dst_file.write(content)
         except FileNotFoundError:
             return
