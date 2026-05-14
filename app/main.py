3ae365968f034d28ff8f8f6143ee4ca53af2182a
Gena <genada89@gmail.com>
2026-05-14 16:04:57 +0300

    fix conversation

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 def copy_file(command: str) -> None:
     try:
-        command, original_file, copy_file = command.split()
+        com, original_file, copy_file = command.split()
     except ValueError as e:
         print(f"Invalid command format: {e}")
         return
 
-    if command == "cp" and original_file != copy_file:
+    if com == "cp" and original_file != copy_file:
         try:
             with open(original_file, "r") as original_file, open(
                 copy_file, "w"
