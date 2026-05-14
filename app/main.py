015754a50e5e7b4a70aa5f024809b6dc605e3bd7
Gena <genada89@gmail.com>
2026-05-14 16:00:26 +0300

    create function

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+def copy_file(command: str) -> None:
+    try:
+        command, original_file, copy_file = command.split()
+    except ValueError as e:
+        print(f"Invalid command format: {e}")
+        return
+
+    if command == "cp" and original_file != copy_file:
+        try:
+            with open(original_file, "r") as original_file, open(
+                copy_file, "w"
+            ) as copy_file:
+                content = original_file.read()
+                copy_file.write(content)
+        except FileNotFoundError:
+            return
